9454cc0f8a60f9c7d73fde7a9c4a046825781df2
Boubacar Fall <boubsfal@gmail.com>
2023-10-27 22:06:30 +0000

    fixing bugs related to renamed variable

--- src/components/Bodyparts.jsx
+++ src/components/Bodyparts.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 
 const Bodyparts = ({ apiUrl, setApiUrl }) => {
   const [bodyparts, setBodyparts] = useState();
@@ -6,37 +6,36 @@ const Bodyparts = ({ apiUrl, setApiUrl }) => {
   const [error, setError] = useState(null);
 
   const fetchData = async () => {
-    const check = localStorage.getItem("bodyparts");
-    if (check) {
-      setBodyparts(JSON.parse(check));
-    } else {
-      const url = `https://exercisedb.p.rapidapi.com/exercises/bodyPartList`;
-      const options = {
-        method: "GET",
-        headers: {
-          "X-RapidAPI-Key":
-            "576b964097msh5dc0dba764788a6p11a705jsn323787f267b6",
-          "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
-        },
-      };
+    // const check = localStorage.getItem("bodyparts");
+    // if (check) {
+    // setBodyparts(JSON.parse(check));
+    // } else {
+    const url = `https://exercisedb.p.rapidapi.com/exercises/bodyPartList`;
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': '576b964097msh5dc0dba764788a6p11a705jsn323787f267b6',
+        'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+      },
+    };
 
-      try {
-        const response = await fetch(url, options);
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        const result = await response.json();
-        setBodyparts(result);
-        // console.log(result);
-        console.log(category);
-        setLoading(false);
-        localStorage.setItem("bodyparts", JSON.stringify(result));
-      } catch (error) {
-        console.error(error);
-        setError(error);
-        setLoading(false);
+    try {
+      const response = await fetch(url, options);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
       }
+      const result = await response.json();
+      setBodyparts(result);
+      // console.log(result);
+      // console.log(category);
+      setLoading(false);
+      // localStorage.setItem("bodyparts", JSON.stringify(result));
+    } catch (error) {
+      console.error(error);
+      setError(error);
+      setLoading(false);
     }
+    // }
   };
 
   useEffect(() => {
@@ -63,17 +62,17 @@ const Bodyparts = ({ apiUrl, setApiUrl }) => {
             <div key={bodypart}>
               <p
                 onClick={(e) => {
-                  if (e.target.innerHTML === "lower arms") {
-                    setApiUrl("/bodyPart/lower%20arms?limit=5000");
-                  } else if (e.target.innerHTML === "lower legs") {
-                    setApiUrl("/bodyPart/lower%20legs?limit=5000");
-                  } else if (e.target.innerHTML === "upper arms") {
-                    setApiUrl("/bodyPart/upper%20arms?limit=5000");
-                  } else if (e.target.innerHTML === "upper legs") {
-                    setApiUrl("/bodyPart/upper%20legs?limit=5000");
+                  if (e.target.innerHTML === 'lower arms') {
+                    setApiUrl('/bodyPart/lower%20arms?limit=5000');
+                  } else if (e.target.innerHTML === 'lower legs') {
+                    setApiUrl('/bodyPart/lower%20legs?limit=5000');
+                  } else if (e.target.innerHTML === 'upper arms') {
+                    setApiUrl('/bodyPart/upper%20arms?limit=5000');
+                  } else if (e.target.innerHTML === 'upper legs') {
+                    setApiUrl('/bodyPart/upper%20legs?limit=5000');
                   } else {
                     setApiUrl(
-                      "/bodyPart/" + e.target.innerHTML + "?limit=5000"
+                      '/bodyPart/' + e.target.innerHTML + '?limit=5000'
                     );
                   }
                 }}
